dc1c374eb40830d04b9466a9fa24a3fbe2b347d8
Ravi RT Merugu <rrmerugu@gmail.com>
2019-05-29 01:59:14 +0530

    Update install-browser.sh

--- chrome/install-browser.sh
+++ chrome/install-browser.sh
@@ -2,16 +2,7 @@
 
 CHROME_VERSION="google-chrome-stable"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list \
   && apt-get update -qqy \
   && apt-get -qqy install \
 ${CHROME_VERSION:-google-chrome-stable}
-
-
-# # Set up the Chrome PPA
-# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-# echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-
-# # Update the package list and install chrome
-# apt-get update -y
-# apt-get install -y google-chrome-stable
