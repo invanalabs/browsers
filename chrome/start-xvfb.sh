24793c67f6fed833777cfd326d7ca38188ed436b
Ravi RT Merugu <rrmerugu@gmail.com>
2019-05-29 07:53:29 +0530

    Update start-xvfb.sh

--- chrome/start-xvfb.sh
+++ chrome/start-xvfb.sh
@@ -2,4 +2,6 @@
 # /opt/bin/start-xvfb.sh
 export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DEPTH}"
 rm -f /tmp/.X*lock
+rm -f /tmp/.X11-unix/X1
+rm -f /tmp/.X1
 /usr/bin/Xvfb ${DISPLAY} -screen 0 ${GEOMETRY} -ac +extension RANDR  
