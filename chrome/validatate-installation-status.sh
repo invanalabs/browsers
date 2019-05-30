ed78720e37a4a598df310d9ab05f2cbe65f87b2e
Ravi RT Merugu <rrmerugu@gmail.com>
2019-05-30 06:35:04 +0530

    Create validatate-installation-status.sh

---
+++ chrome/validatate-installation-status.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# this script will pause starting the invana-bot till the selenium is up
+# /validatate-installation-status.sh
+
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:4444/); do
+    printf '.'
+    sleep 1
+done
