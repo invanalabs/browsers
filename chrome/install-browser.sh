635f5fbe73de014211d44b184885ed416a2884aa
Ravi RT Merugu <rrmerugu@gmail.com>
2019-05-30 05:49:34 +0530

    Update install-browser.sh

--- chrome/install-browser.sh
+++ chrome/install-browser.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-apt-get update -y
-apt-get install -y google-chrome-stable
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+# echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+# apt-get update -y
+# apt-get install -y google-chrome-stable
+
+
+CHROME_VERSION="google-chrome-stable"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+  && apt-get update -y \
+  && apt-get -y install ${CHROME_VERSION:-google-chrome-stable}
