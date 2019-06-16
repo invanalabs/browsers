93f795527d4f929f6a03ca1285309dd202170ab5
Ravi RT Merugu <rrmerugu@gmail.com>
2019-06-16 13:18:13 +0530

    Update install-browser.sh

--- chrome/install-browser.sh
+++ chrome/install-browser.sh
@@ -6,8 +6,8 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee -a /etc/apt/sources.list.d/google-chrome.list
 #sudo apt-get update -y && sudo apt-get -y install google-chrome-stable=$CHROME_VERSION
 sudo apt-get update -y 
-sudo curl  -H 'Cache-Control: no-cache' -o /google-chrome-stable_{$CHROME_VERSION}_amd64.deb http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_{$CHROME_VERSION}_amd64.deb 
-sudo dpkg -i /google-chrome-stable_{$CHROME_VERSION}_amd64.deb
+sudo curl  -H 'Cache-Control: no-cache' -o /google-chrome-stable_$CHROME_VERSION_amd64.deb http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_$CHROME_VERSION_amd64.deb 
+sudo dpkg -i /google-chrome-stable_$CHROME_VERSION_amd64.deb
 if [ $? -gt 0 ]; then
 apt-get -f --force-yes --yes install
 fi
